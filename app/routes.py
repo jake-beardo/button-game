14f29247d15de9da3b8a4f48761fc63ebf7ba160
Jake Beard <jake.beard@hotmail.com>
2022-03-07 19:08:09 +0000

    button attempt

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template, flash
 from app import app
-
+from button import button
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    button = button()
+    return render_template('index.html', title='Button Home', button=button)
+
+@app.route('/help')
+def help():
+    return "found help"
