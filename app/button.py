16d399c2ae1fd6df46f930fd79c94f2f814e40e4
Jake Beard <jake.beard@hotmail.com>
2022-04-21 19:38:35 +0100

    Complete plum button game

--- app/button.py
+++
@@ -1,6 +0,0 @@
-import random
-
-words = ['milf', 'monk', 'monkey', 'monster', 'masterbator', 'master of bates', 'melanin','menstral cycle', 'man', 'mouse']
-def button():
-    word = random.choice(words)
-    return f"You are a {word}"
